a94e976db0923219d8ec2023d074b25b289f8bbd
Noam Korkos <noam6421@gmail.com>
2020-04-17 19:57:50 +0300

    Init commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTO_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MSEEAGING_SENDER_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { firebase, googleAuthProvider, database as default };
